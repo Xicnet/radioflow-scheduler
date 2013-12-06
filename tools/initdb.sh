9d4777a00a2c9c66edbe200cfe878dafd0f9ba01
root <root@rna.vz.lan>
2013-12-06 12:29:31 -0300

    Do migration and load Day model from fixture on initdb script

--- tools/initdb.sh
+++ tools/initdb.sh
@@ -1,3 +1,5 @@
 bin/django syncdb --noinput
+bin/django syncdb --migrate
 bin/django loaddata fixture/auth.user.json
+bin/django loaddata fixture/timeslot.day.json
 bin/django loaddata fixture/timeslot.program.json
