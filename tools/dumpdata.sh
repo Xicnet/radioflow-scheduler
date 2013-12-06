e1471e083a42f32c7a7e4ef04ca06691431ecd27
Ramiro Augusto Cosentino <rama@xicnet.com>
2013-12-06 12:25:30 -0300

    Add dumpdata script

---
+++ tools/dumpdata.sh
@@ -0,0 +1,4 @@
+bin/django dumpdata timeslot.program > fixture/timeslot.program.json 
+bin/django dumpdata timeslot.day > fixture/timeslot.day.json 
+bin/django dumpdata auth.User > fixture/auth.user.json 
+
