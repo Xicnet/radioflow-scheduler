9c4db42534a4b00312e311153204303fd3a44b52
Ramiro Augusto Cosentino <rama@xicnet.com>
2013-12-12 19:00:29 -0300

    Cleanup

--- project/timeslot/forms.py
+++ project/timeslot/forms.py
@@ -8,9 +8,6 @@ from ajax_upload.widgets import AjaxClearableFileInput
 class ProgramForm(forms.Form):
     def clean_name(self):
         data = self.cleaned_data['name']
-        #streamer = Streamer.objects.filter(name=data)
-        #if streamer.count() > 0:
-        #    raise forms.ValidationError("You can only register one time!")
         return data
 
     def clean_image(self):
@@ -27,11 +24,6 @@ class ProgramForm(forms.Form):
     name      = forms.CharField(label="Nombre", max_length=256, min_length=1, required=True, error_messages={'max_length': 'Por favor, ingrese menos caracteres (256 max)'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
     description = TextAreaField(label="Detalle", max_length=1024, min_length=1, required=False, error_messages={'max_length': 'Por favor, ingrese menos caracteres (1024 max)'}, widget=forms.Textarea(attrs={'class':'suscription-textarea'}), help_text="1024 caracteres")
     moderator = forms.CharField(label="Conduce", max_length=1024, min_length=1, required=False, error_messages={'max_length': 'Por favor, ingrese menos caracteres (1024 max)'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #facebook  = forms.URLField(max_length=512, min_length=1, required=False, error_messages={'max_length': 'Please enter less characters'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #twitter   = forms.URLField(max_length=512, min_length=1, required=False, error_messages={'max_length': 'Please enter less characters'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #web       = forms.URLField(max_length=512, min_length=1, required=False, error_messages={'max_length': 'Please enter less characters'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #email     = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #days      = forms.SelectMultiple(choices=('1980', '1981', '1982'))
     days      = forms.ModelMultipleChoiceField(label="Dias", queryset=Day.objects.all(), widget=forms.CheckboxSelectMultiple)
     start     = forms.TimeField(label="Comienzo", required=False, help_text="Formato HH:MM. Ej: 12:30 o 00:30")
     end       = forms.TimeField(label="Fin", required=False, help_text="Formato HH:MM. Ej: 12:30 o 00:30")
@@ -47,11 +39,6 @@ class ConfigForm(forms.Form):
         return image
 
     streamurl = forms.CharField(label="URL Stream", max_length=512, min_length=1, required=True, error_messages={'max_length': 'Por favor, ingrese menos caracteres (256 max)'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #facebook  = forms.URLField(max_length=512, min_length=1, required=False, error_messages={'max_length': 'Please enter less characters'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #twitter   = forms.URLField(max_length=512, min_length=1, required=False, error_messages={'max_length': 'Please enter less characters'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #web       = forms.URLField(max_length=512, min_length=1, required=False, error_messages={'max_length': 'Please enter less characters'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #email     = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    #days      = forms.SelectMultiple(choices=('1980', '1981', '1982'))
     image     = forms.ImageField(label="Imagen", required=False, help_text="")
 
 
