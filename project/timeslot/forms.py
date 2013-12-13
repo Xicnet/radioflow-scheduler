9dc28b51e0eb46d39d249ecfe297a1b349283674
Ramiro Augusto Cosentino <rama@xicnet.com>
2013-12-12 21:05:44 -0300

    Minor

--- project/timeslot/forms.py
+++ project/timeslot/forms.py
@@ -21,9 +21,9 @@ class ProgramForm(forms.Form):
     days = Day.objects.all()
 
     name      = forms.CharField(label="Nombre", max_length=256, min_length=1, required=True, error_messages={'max_length': 'Por favor, ingrese menos caracteres (256 max)'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
-    moderator = forms.CharField(label="Conduce", max_length=1024, min_length=1, required=False, error_messages={'max_length': 'Por favor, ingrese menos caracteres (1024 max)'}, widget=forms.TextInput(attrs={'class':'suscription-input'}))
+    moderator = forms.CharField(label="Conduce", max_length=1024, min_length=1, required=False, error_messages={'max_length': 'Por favor, ingrese menos caracteres (1024 max)'}, widget=forms.TextInput(attrs={'class':'suscription-input'}), help_text="(Opcional)")
     image     = forms.ImageField(label="Imagen", required=False, help_text="JPG/PNG a 320x125 o 640x250")
-    description = TextAreaField(label="Detalle", max_length=1024, min_length=1, required=False, error_messages={'max_length': 'Por favor, ingrese menos caracteres (1024 max)'}, widget=forms.Textarea(attrs={'class':'suscription-textarea'}), help_text="1024 caracteres (opcional)")
+    description = TextAreaField(label="Detalle", max_length=1024, min_length=1, required=False, error_messages={'max_length': 'Por favor, ingrese menos caracteres (1024 max)'}, widget=forms.Textarea(attrs={'class':'suscription-textarea'}), help_text="1024 caracteres (Opcional)")
     days      = forms.ModelMultipleChoiceField(label="Dias", queryset=Day.objects.all(), widget=forms.CheckboxSelectMultiple)
     start     = forms.TimeField(label="Comienzo", required=True, help_text="Formato HH:MM. Ej: 12:30 o 00:30")
     end       = forms.TimeField(label="Fin", required=True, help_text="Formato HH:MM. Ej: 12:30 o 00:30")
