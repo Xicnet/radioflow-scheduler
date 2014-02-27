ad4de939dbbeea35d46ac6f49eebdbfc2b39aea9
Ramiro A. Cosentino <code@xicnet.com>
2014-02-27 11:47:17 -0300

    Add Config model to load/dump scripts

--- tools/dumpdata.sh
+++ tools/dumpdata.sh
@@ -1,4 +1,4 @@
 bin/django dumpdata timeslot.program > fixture/timeslot.program.json 
 bin/django dumpdata timeslot.day > fixture/timeslot.day.json 
+bin/django dumpdata timeslot.config > fixture/timeslot.config.json 
 bin/django dumpdata auth.User > fixture/auth.user.json 
-
