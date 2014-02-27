2de3355762cd0df7bb38f8289f0a3b98d687ecd4
Ramiro Augusto Cosentino <rama@xicnet.com>
2014-02-27 11:47:58 -0300

    Minor

--- tools/initdb.sh
+++ tools/initdb.sh
@@ -3,3 +3,4 @@ bin/django syncdb --migrate
 bin/django loaddata fixture/auth.user.json
 bin/django loaddata fixture/timeslot.day.json
 bin/django loaddata fixture/timeslot.program.json
+bin/django loaddata fixture/timeslot.config
