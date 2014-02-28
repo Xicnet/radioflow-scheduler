a49c5d6e7e15d96edde1832330ca0f5081a4d67e
Ramiro Augusto Cosentino <rama@xicnet.com>
2014-02-27 21:31:26 -0300

    Add xere to sites

--- tools/initdb.sh
+++ tools/initdb.sh
@@ -3,4 +3,8 @@ bin/django syncdb --migrate
 bin/django loaddata fixture/auth.user.json
 bin/django loaddata fixture/timeslot.day.json
 bin/django loaddata fixture/timeslot.program.json
-bin/django loaddata fixture/timeslot.config
+bin/django loaddata fixture/timeslot.config.json
+if [ "`hostname`" == "xere" ]
+then
+	bin/django loaddata fixture/sites.xere.json
+fi
