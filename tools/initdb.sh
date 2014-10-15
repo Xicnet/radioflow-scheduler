a9d0b05027c10f52721d02695d2721d21c9d9895
Ramiro Augusto Cosentino <rama@xicnet.com>
2014-10-14 22:39:18 -0300

    Fix initial DB installer

--- tools/initdb.sh
+++ tools/initdb.sh
@@ -1,9 +1,9 @@
 bin/django syncdb --noinput
 bin/django syncdb --migrate
-bin/django loaddata fixture/auth.user.json
+bin/django loaddata fixture/auth.user.admin.json
 bin/django loaddata fixture/timeslot.day.json
-bin/django loaddata fixture/timeslot.program.json
-bin/django loaddata fixture/timeslot.config.json
+#bin/django loaddata fixture/timeslot.program.json
+#bin/django loaddata fixture/timeslot.config.json
 if [ "`hostname`" == "xere" ]
 then
 	bin/django loaddata fixture/sites.xere.json
