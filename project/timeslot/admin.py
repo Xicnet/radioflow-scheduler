989a614c4d03ee2db210cf5524442d12a64f93a2
Ramiro Augusto Cosentino <rama@xicnet.com>
2016-06-16 03:08:19 -0300

    Tweak config admin

--- project/timeslot/admin.py
+++ project/timeslot/admin.py
@@ -1,10 +1,15 @@
-from timeslot.models import Program, Day, Config
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from timeslot.models import Program, Day, Config
 from image_cropping import ImageCroppingMixin
 
 class ProgramAdmin(ImageCroppingMixin, admin.ModelAdmin):
     list_display    = ('name', 'moderator', 'start', 'end')
 
+class ConfigAdmin(admin.ModelAdmin):
+    list_display    = ('station', 'user')
+
 admin.site.register(Program, ProgramAdmin)
 admin.site.register(Day)
-admin.site.register(Config)
+admin.site.register(Config, ConfigAdmin)
