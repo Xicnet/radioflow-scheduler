dcf22e77c7beab31dc9470d3acbf12611fd2344f
Ramiro A. Cosentino <code@xicnet.com>
2016-06-26 13:18:42 -0300

    Change facebook URL to use fb://page instead of fb://profile

--- project/utils/__init__.py
+++ project/utils/__init__.py
@@ -7,7 +7,7 @@ from urlparse import urlparse
 def get_fb_profile_url(url):
     name = urlparse(url).path.strip("/")
     graph = facebook.GraphAPI(access_token=settings.FB_ACCESS_TOKEN)
-    return "fb://profile/%s" % graph.get_object(name)['id']
+    return "fb://page/%s" % graph.get_object(name)['id']
 
 def parse_tw_url(url):
     return urlparse(url).path.strip("/")
