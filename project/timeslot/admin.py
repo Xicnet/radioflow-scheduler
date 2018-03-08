359a15eac062b94beb3ecd4699e2f3eddfe6253e
Rama <code@xicnet.com>
2018-03-08 14:57:52 +0000

    Implementing log to program matching

--- project/timeslot/admin.py
+++ project/timeslot/admin.py
@@ -5,7 +5,9 @@ from timeslot.models import Program, Day, Config
 from image_cropping import ImageCroppingMixin
 
 class ProgramAdmin(ImageCroppingMixin, admin.ModelAdmin):
-    list_display    = ('name', 'user', 'moderator', 'start', 'end')
+    list_display    = ('name', 'user', 'moderator', 'start', 'end',)
+    search_fields    = ('name',)
+    list_filter    = ('user',)
 
 class ConfigAdmin(admin.ModelAdmin):
     list_display    = ('station', 'user', 'streamurl', 'facebook', 'twitter', 'web', 'email')
